9b758b08089c5a132a44d427b1ecc5c93e3fc189
MishaelTech <mishaelchinomso@gmail.com>
2024-08-01 15:26:38 +0300

    set a timer, score, terrain environment, and modified the game play

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,6 +10,9 @@ public class PlayerController : MonoBehaviour
 
     float xInput;
     float yInput;
+    float timer = 0f;
+
+    bool gameStarted = false;
 
     int score = 0;
     public int winScore;
@@ -20,11 +20,14 @@ public class PlayerController : MonoBehaviour
     public int playerScore;
 
     public GameObject winText;
-    public TextMeshProUGUI highText;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI scoreText;
 
     private void Start()
     {
         highScore = PlayerPrefs.GetInt("HighScore", highScore);
+        UpdateScoreText();
+        StartGame();
     }
 
     private void Awake()
@@ -35,22 +38,51 @@ public class PlayerController : MonoBehaviour
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
-        playerScore = playerScore + scoreToAdd;
+        playerScore += scoreToAdd;
+        UpdateScoreText();
+        /*playerScore = playerScore + scoreToAdd;
+        scoreText.text = $"Score: {playerScore}";*/
+    }
+
+    void UpdateScoreText()
+    {
+        // Avoid division by zero if highScore is zero
+        scoreText.text = $"Score: {playerScore} / {highScore}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < -5f)
+        if(transform.position.y < -0f)
         {
             SceneManager.LoadScene("Game");
         }
 
-        if(playerScore > highScore)
+        if (gameStarted)
+        {
+            timer += Time.deltaTime;
+            timerText.text = $"Time: {timer:F2}"; // Display the timer in seconds with 2 decimal places
+
+            if (timer >= 30f)
+            {
+                RestartGame();
+            }
+        }
+
+        /* if (playerScore > highScore)
+         {
+             highScore = playerScore;
+             PlayerPrefs.SetInt("HighScore", highScore);
+             PlayerPrefs.Save(); // Ensure changes are saved immediately
+         }
+
+         highText.text = $"HighScore: {highScore}";*/
+
+        /*if(playerScore > highScore)
         {
             PlayerPrefs.SetInt("HighScore", playerScore);
         }   
-        highText.text = $"HighScore: {PlayerPrefs.GetInt("HighScore", 0)}";
+        highText.text = $"HighScore: {PlayerPrefs.GetInt("HighScore", 0)}";*/
     }
 
     private void FixedUpdate()
@@ -73,13 +105,30 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
 
             score++;
+            addScore(1);
 
             if(score >= winScore)
             {
                 //gamewin
                 winText.SetActive(true);
+                gameStarted = false; 
                 Invoke(nameof(RestartGame), 5f);
             }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            transform.position = new Vector3(0, 2.17f, 0);
+        }
+    }
+
+    // Call this method to start the timer when the game begins
+    public void StartGame()
+    {
+        timer = 0f;
+        gameStarted = true;
     }
 }
